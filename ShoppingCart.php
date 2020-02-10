9e47644d22620254fbe622f0989bf311023f202a
Nathaniel Pather <nathaniel_pather@hotmail.com>
2020-02-10 13:47:17 +1000

    Add listProducts function.

--- ShoppingCart.php
+++ ShoppingCart.php
@@ -1,10 +1,6 @@
 <?php
 error_reporting(E_STRICT);
 
-class ShoppingCart {
-
-}
-
 class Cart {
     function add() {
 
@@ -24,5 +20,23 @@ class Product {
 }
 
 class Products {
+    public static function listProducts() {
+        // ######## please do not alter the following code ########
+        $products = [
+            ["name" => "Sledgehammer", "price" => 125.75],
+            ["name" => "Axe", "price" => 190.50],
+            ["name" => "Bandsaw", "price" => 562.131],
+            ["name" => "Chisel", "price" => 12.9],
+            ["name" => "Hacksaw", "price" => 18.45],
+        ];
+        // ########################################################
 
+        for($i = 0; $i < count($products); $i++) {
+            echo $products[$i]["name"].": ".$products[$i]["price"].PHP_EOL;
+
+        }
+    }
 }
+
+$a = new Products();
+$a->listProducts();
